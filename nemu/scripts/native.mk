6e9e5f49a918309cc762761323eb0260e27937b5
crx <rxchen1999@163.com>
2026-09-01 19:23:02 +0800

    gdb: 关闭 debuginfod 自动下载系统库调试信息

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -40,7 +40,7 @@ run: run-env
 
 gdb: run-env
 # 	$(call git_commit, "gdb NEMU")
-	gdb -s $(BINARY) --args $(NEMU_EXEC)
+	gdb -iex "set debuginfod enabled off" -s $(BINARY) --args $(NEMU_EXEC)
 
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
